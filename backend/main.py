2541f111c766e173ecf1234e11c1ff2d122f5cc1
Nick Cleary <nicholascleary01@gmail.com>
2023-02-14 01:10:30 -0500

    Adding redis and pytest

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from watchtower.thingy import a_func
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    a_func()
     return {"message": "Hello World"}
+
+def a_func2():
+    print("hi")
---
+++ backend/tests/initial_test.py
@@ -0,0 +1,4 @@
+from watchtower.thingy import a_func
+
+def test_thing():
+    assert a_func() is None
